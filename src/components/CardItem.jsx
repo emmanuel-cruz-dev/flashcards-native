d7d571020d8d2a9396ddaa61921e6dbb78aa5bf7
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-30 12:18:55 -0300

    funcionalidad en CardItem

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,13 +1,21 @@
 import { Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useModal } from "../hooks/useModal";
 
 function CardItem(card) {
-  const [visible, toggle] = useModal(true);
+  const { visible, toggle, show } = useModal(true);
   const { front, back, detail } = card;
 
+  useEffect(() => {
+    show();
+  }, [card]);
+
   return (
-    <TouchableOpacity onPress={toggle}>
+    <TouchableOpacity
+      onPress={toggle}
+      activeOpacity={0.5}
+      style={{ backgroundColor: "#f1aab2" }}
+    >
       {visible ? (
         <Text>{front}</Text>
       ) : (
