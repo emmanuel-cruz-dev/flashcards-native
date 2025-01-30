8fa4d71bd3d08ece83cab734d5378a55fb37d992
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-30 08:38:05 -0300

    AddNewCard contenido agregado

---
+++ src/components/AddNewCard.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+function AddNewCard() {
+  return (
+    <View>
+      <Text>Add NewCard</Text>
+    </View>
+  );
+}
+
+export default AddNewCard;
