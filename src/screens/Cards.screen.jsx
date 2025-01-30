efb08b3fd6e23101a7664f2f65590aa6d3c322c1
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-30 11:07:42 -0300

    renderizado en base a numero de tarjetas y tarjeta id en Cards screen

--- src/screens/Cards.screen.jsx
+++ src/screens/Cards.screen.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
 import { Text, View } from "react-native";
 import AddNewCard from "../components/AddNewCard";
 import { useCards } from "../hooks/data";
+import { Button } from "@rneui/base";
 
 function Cards() {
   const route = useRoute();
   const { category } = route.params;
   const cards = useCards(category.id);
-  const [activeCard, setActiveCard] = useState(null);
+  const [active, setActive] = useState(null);
+
+  const showNewCard = () => {};
+
+  useEffect(() => {
+    if (!cards.length) return;
+    if (cards.length === 1) setActive(cards[0].id);
+  }, [cards]);
+
+  const activeCard = cards.find((card) => card.id === active);
 
   return (
     <View>
       <AddNewCard></AddNewCard>
       <Text>Category: {category.name}</Text>
 
-      {cards.map((card) => (
-        <Text key={card.id}>{card.front}</Text>
-      ))}
+      {!cards.length && <Text>Try adding a new card</Text>}
+      {active && <Text>{activeCard.front}</Text>}
+
+      <Button title={"Choose new"} onPress={showNewCard} />
     </View>
   );
 }
