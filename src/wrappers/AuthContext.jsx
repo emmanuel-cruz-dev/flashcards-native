94d46747885894e721b05b2ce409d764687488d2
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-30 08:34:42 -0300

    AuthContext agregado

---
+++ src/wrappers/AuthContext.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../api/db";
+
+export const AuthContext = createContext();
+
+export function AuthContextWrapper({ children }) {
+  const value = useState(null);
+  const [user, setUser] = value;
+
+  useEffect(() => {
+    if (auth.currentUser && !user) {
+      setUser(auth.currentUser);
+    }
+  }, [setUser, user]);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
