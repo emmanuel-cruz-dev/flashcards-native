deb7e044cfef3b65d694adf03abb4aa234232756
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-31 10:43:05 -0300

    text en util cambiado

--- src/utils/text.js
+++ src/utils/text.js
@@ -1,2 +1,5 @@
-export const pluralize = ({ noun, number }) =>
-  `${number || "No"} ${noun}${number === 1 ? "" : "s"}`;
+// export const pluralize = ({ noun, number }) =>
+//   `${number || "No"} ${noun}${number === 1 ? "" : "s"}`;
+
+export const pluralize = ({ text = "", quantity = 0 }) =>
+  `${quantity || "No"} ${text}${quantity === 1 ? "" : "s"}`;
